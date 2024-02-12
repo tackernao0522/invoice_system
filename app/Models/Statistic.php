06297366f48ad7e7c079b5cbfd4b5688801eb6df
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-02-12 18:12:26 +0900

    statistic(統計情報)関連のテーブル及びモデルの作成

---
+++ app/Models/Statistic.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Statistic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'emloyee_id',
+        'sales_amount',
+        'date',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
